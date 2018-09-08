46da2cab5148c04773b9deeb1a97d4baa5beb7b0
jenkins <jenkins@nalkins.cloud>
2018-09-08 23:19:42 +0300

    update version to: 0.0.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nalkinscloud-api',
-    version='0.0.13',
+    version='0.0.14',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
