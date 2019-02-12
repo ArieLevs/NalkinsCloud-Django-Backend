22e932ab5bf86e42748c918327925c07b1d31d56
arielev <levinsonarie@gmail.com>
2019-02-12 22:22:25 +0200

    Update non production host name

--- src/nalkinscloud_django/settings.py
+++ src/nalkinscloud_django/settings.py
@@ -22,7 +22,7 @@ else:
     DEBUG = False
 
 ALLOWED_HOSTS = ['api.nalkinscloud.localhost',
-                 'api-alpha.nalkins.cloud',
+                 'alpha.api.nalkins.cloud',
                  'api.nalkins.cloud',
                  '127.0.0.1',
                  '10.0.2.2']  # Android AVD IP for localhost
