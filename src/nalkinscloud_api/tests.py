e4d7d4b3cea3422efc59b12db9fa6372979a0ece
arielev <levinsonarie@gmail.com>
2019-04-17 01:33:54 +0300

    add broken functions unit tests

--- src/nalkinscloud_api/tests.py
+++ src/nalkinscloud_api/tests.py
@@ -6,16 +6,17 @@ from django.utils import timezone
 from oauth2_provider.models import AccessToken
 from rest_framework.test import APITestCase
 from nalkinscloud_api.functions import *
+from nalkinscloud_mosquitto.functions import *
 import datetime
 import logging
 from nalkinscloud_django.settings import PROJECT_NAME
+from nalkinscloud_mosquitto.models import Device
 
 # Define logger
 logger = logging.getLogger(PROJECT_NAME)
 
 
-class TestFunctions(TestCase):
-
+class TestAPIFunctions(TestCase):
     def setUp(self):
         self.oauth_client_id = 'some_client_id'
         self.oauth_client_secret = 'some_client_secret'
@@ -64,6 +65,75 @@ class TestFunctions(TestCase):
     def test_is_client_secret_exists(self):
         self.assertTrue(is_client_secret_exists(client_secret=self.oauth_client_secret))
         self.assertFalse(is_client_secret_exists(client_secret='some_other_client_secret'))
+
+
+class TestBrokerFunctions(TestCase):
+    def setUp(self):
+        self.username = 'some_username'
+        self.email = "test@nalkins.cloud"
+        self.password = "nalkinscloud"
+
+        self.user = User.objects.create_user(email=self.email, password=self.password,
+                                             user_name=self.username)
+
+        self.device_id = 'some_device_id'
+        self.device_password = 'nalkinscloud'
+        self.device_model = 'application'
+        self.device_type = 'user'
+
+        self.device = Device.objects.create_device(device_id=self.device_id, password=self.device_password,
+                                                   model=DeviceModel.objects.get(model=self.device_model),
+                                                   type=DeviceType.objects.get(type=self.device_type))
+
+        self.topic = 'some/important/topic'
+        # Generate access list record
+        # AccessList.objects.get_or_create(device_id=self.device_id, topic=self.topic)
+
+    def test_is_device_id_exists(self):
+        self.assertTrue(is_device_id_exists(self.device_id))
+        self.assertFalse(is_device_id_exists('non_existing_device_id'))
+
+    def test_update_device_pass_mosquitto_db(self):
+        self.assertTrue(update_device_pass_mosquitto_db(self.device_id, 'new_password'))
+        self.assertFalse(update_device_pass_mosquitto_db('non_existing_device_id', 'new_password'))
+
+    def test_insert_into_acls_mosquitto_db(self):
+        self.assertFalse(AccessList.objects.filter(device_id=self.device_id, topic='some/other/topic').exists())
+        insert_into_acls_mosquitto_db(device_id=self.device_id, topic='some/other/topic')
+        self.assertTrue(AccessList.objects.filter(device_id=self.device_id, topic='some/other/topic').exists())
+
+    def test_get_device_owner(self):
+        self.assertFalse(get_device_owner(device_id=self.device_id, logged_in_user_id=self.user.email),
+                         "Should return False since current user does not have any devices")
+        # Add current user, current device, and test again
+        CustomerDevice.objects.create(user_id=self.user, device_id=self.device)
+        self.assertTrue(get_device_owner(device_id=self.device_id, logged_in_user_id=self.user.email))
+        CustomerDevice.objects.filter(user_id=self.user, device_id=self.device).delete()
+
+    def test_insert_into_customer_devices(self):
+        self.assertTrue(insert_into_customer_devices(user_id=self.user,
+                                                     device_id=self.device,
+                                                     device_name='test_device_name'))
+        self.assertFalse(insert_into_customer_devices(user_id=self.user,
+                                                      device_id=self.device,
+                                                      device_name='test_device_name'),
+                         "Should return False since already exists")
+        CustomerDevice.objects.filter(user_id=self.user, device_id=self.device).delete()
+
+    def test_remove_from_customer_devices(self):
+        CustomerDevice.objects.create(user_id=self.user, device_id=self.device)
+        self.assertTrue(CustomerDevice.objects.filter(user_id=self.user, device_id=self.device).exists())
+
+        remove_from_customer_devices(user_id=self.user, device_id=self.device)
+        self.assertFalse(CustomerDevice.objects.filter(user_id=self.user, device_id=self.device).exists())
+
+    def test_remove_from_acls(self):
+        AccessList.objects.create(device_id=self.device_id, topic='some/remove_acl_test/topic')
+        self.assertTrue(AccessList.objects.filter(device_id=self.device_id,
+                                                  topic='some/remove_acl_test/topic').exists())
+        remove_from_acls(device_id=self.device_id, topic='some/remove_acl_test/topic')
+        self.assertFalse(AccessList.objects.filter(device_id=self.device_id,
+                                                   topic='some/remove_acl_test/topic').exists())
 
 
 class APIViewsTestCase(APITestCase):
@@ -140,5 +210,4 @@ class APIViewsTestCase(APITestCase):
     def test_device_list_view(self):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.access_token))
         response = self.client.post(self.device_list_url)
-        logger.debug('test_device_list_view response: ' + str(response.json()))
         self.assertEqual(204, response.status_code, "Should return empty list")
